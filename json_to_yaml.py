fbcceb02b9aab0b2d1b340fbafe694932f38aa97
Bohe <brianbohe@gmail.com>
2018-07-01 05:58:18 -0300

    rename main file. using stdin

---
+++ json_to_yaml.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+from ply.lex import lex
+from ply.yacc import yacc
+
+import tokrules
+import parser_rules
+
+def read():
+    # build athe json in a string 
+    json = ""
+    
+    for line in sys.stdin:
+        json += line
+
+    return json
+
+if __name__ == "__main__":
+    expr = read()
+
+    lexer = lex(module=tokrules)
+    parser = yacc(module=parser_rules)
+    
+    ast = parser.parse(expr, lexer)
+    print(ast.yaml())
