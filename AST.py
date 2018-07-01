9a29c2169a78bd334455bc555e90168baff18b05
jessicasinger <singer.jeess@gmail.com>
2018-06-30 21:12:46 -0300

    cambiar grmatica json

---
+++ AST.py
@@ -0,0 +1,161 @@
+class Expr: pass
+
+class LambdaExpr(Expr):
+    def __init__(self):
+        pass
+
+    def __eq__(self, other):
+        return isinstance(other, LambdaExpr)
+
+    def __str__(self):
+        return "LambdaExpr"
+
+    def accept(self, visitor):
+        visitor.visitLambda(self)
+
+# a
+class Chr(Expr):
+
+    non_character_error_description = "Character should have length one"
+
+    def __init__(self, character):
+        if len(character) != 1:
+            raise ValueError(Chr.non_character_error_description)
+        self.character = character
+
+    def __eq__(self, other):
+        if isinstance(other, Chr):
+            return other.character == self.character
+        else:
+            return False
+
+    def __str__(self):
+        return "Chr({0})".format(self.character)
+
+    def accept(self, visitor):
+        visitor.visitChr(self)
+
+class DivExpr(Expr):
+    def __init__(self, leftExpr, rightExpr):
+        self.leftExpr = leftExpr
+        self.rightExpr = rightExpr
+
+    def __eq__(self, other):
+        if not isinstance(other, DivExpr):
+            return False
+
+        return self.leftExpr == other.leftExpr and self.rightExpr == other.rightExpr
+    def __str__(self):
+        return "DivExpr({0},{1})".format(self.leftExpr, self.rightExpr)
+
+    def accept(self, visitor):
+        visitor.visitDiv(self)
+
+# A B
+class Concat(Expr):
+    def __init__(self, leftExpression, rightExpression):
+        self.leftExpression = leftExpression
+        self.rightExpression = rightExpression
+
+    def __eq__(self, other):
+        if not isinstance(other, Concat):
+            return False
+        return self.leftExpression == other.leftExpression and self.rightExpression == other.rightExpression
+
+    def __str__(self):
+        return "Concat({0},{1})".format(self.leftExpression, self.rightExpression)
+
+    def accept(self, visitor):
+        visitor.visitConcat(self)
+
+class SuperSub(Expr):
+    def __init__(self, mainExpr, superExpr, subExpr):
+        self.mainExpr = mainExpr
+        self.superExpr = superExpr
+        self.subExpr = subExpr
+
+    def __eq__(self, other):
+        if not isinstance(other, SuperSub):
+            return False
+
+        comp = True
+        comp = comp and (self.mainExpr == other.mainExpr)
+        comp = comp and (self.superExpr == other.superExpr)
+        comp = comp and (self.subExpr == other.subExpr)
+
+        return comp
+
+    def __str__(self):
+        return "SuperSub({0}, {1}, {2})".format(str(self.mainExpr), str(self.superExpr), str(self.subExpr))
+
+    def accept(self, visitor):
+        visitor.visitSuperSub(self)
+
+class SubSuper(Expr):
+    def __init__(self, mainExpr, subExpr, superExpr):
+        self.mainExpr = mainExpr
+        self.superExpr = superExpr
+        self.subExpr = subExpr
+
+    def __eq__(self, other):
+        if not isinstance(other, SubSuper):
+            return False
+
+        comp = True
+        comp = comp and (self.mainExpr == other.mainExpr)
+        comp = comp and (self.superExpr == other.superExpr)
+        comp = comp and (self.subExpr == other.subExpr)
+
+        return comp
+
+    def __str__(self):
+        return "SubSuper({0}, {1}, {2})".format(str(self.mainExpr), str(self.superExpr), str(self.subExpr))
+
+    def accept(self, visitor):
+        visitor.visitSubSuper(self)
+
+class SuperSuffix(Expr):
+    def __init__(self, expr):
+        self.expr = expr
+
+    def __eq__(self, other):
+        if not isinstance(other, SuperSuffix):
+            return False
+        return other.expr == self.expr
+
+    def __str__(self):
+        return "SuperSuffix({0})".format(self.expr)
+
+    def accept(self, visitor):
+        visitor.visitSuperSuffix(self)
+
+class SubSuffix(Expr):
+    def __init__(self, expr):
+        self.expr = expr
+
+    def __eq__(self, other):
+        if not isinstance(other, SubSuffix):
+            return False
+        return other.expr == self.expr
+
+    def __str__(self):
+        return "SubSuffix({0})".format(self.expr)
+
+    def accept(self, visitor):
+        visitor.visitSubSuffix(self)
+
+# ()
+class GroupedPar(Expr):
+    def __init__(self, expr):
+        self.expr = expr
+
+    def __eq__(self, other):
+        if not isinstance(other, GroupedPar):
+            return False
+        return self.expr == other.expr
+
+    def __str__(self):
+        return "GroupedPar({0})".format(self.expr)
+
+    def accept(self, visitor):
+        visitor.visitGroupedPar(self)
---
+++ main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+from sys import argv, exit
+import argparse
+
+from ply.lex import lex
+import tokrules
+
+from ply.yacc import yacc
+import parser_rules
+
+import AST_visitors
+
+def ast_generate(input_str):
+    lexer = lex(module=tokrules)
+    parser = yacc(module=parser_rules)
+    ast = parser.parse(input_str, lexer)
+    return ast
+
+def generar(input):
+    ast = ast_generate(input)
+    print(ast)
+    ast.accept(AST_visitors.EscaleVisitor(1))
+    ast.accept(AST_visitors.WidthVisitor())
+    ast.accept(AST_visitors.HVisitor())
+    ast.accept(AST_visitors.XVisitor(0))
+    ast.accept(AST_visitors.YVisitor(ast.h1))
+    ast.accept(AST_visitors.SVGRendererVisitor())
+
+    return ast
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description='Parsea y genera SVG. Lo podes mandar a un archivo')
+    argparser.add_argument('expression', type=str,
+                        help='Expresion para parsear')
+
+    argparser.add_argument('--output', '-o', dest='output_filename', default='output.svg', type=str,
+                        help="Archivo output svg")
+
+    argparser.add_argument('--print-svg', '-p', dest='print_svg', action='store_const',
+                    const=True, default=False,
+                    help='Imprimir svg')
+
+    args = argparser.parse_args()
+
+    ast_with_attributes = generar(args.expression)
+    result = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"{}\" height=\"{}\" version=\"1.1\" style=\"background: white\">\n<g transform=\"scale(40) translate(1,1)\" font-family=\"Courier\">\n".format(ast_with_attributes.a*50+80, (ast_with_attributes.h1+ast_with_attributes.h2)*40+80, ast_with_attributes.h1)
+    result += ast_with_attributes.svg
+    result += "</g>\n</svg>\n"
+
+    output_file = open(args.output_filename, "w")
+    output_file.write(result)
+    output_file.close()
+
+    if args.print_svg:
+        print(result)
